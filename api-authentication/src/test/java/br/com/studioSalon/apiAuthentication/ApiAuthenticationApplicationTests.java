19adea67d96bb0bd03c58ec8b0d1f1672c48f69a
Wanderson Lira <wandersonlira.dev1@gmail.com>
2025-01-11 11:47:42 -0300

    :sparkles: feat: Código fonte funcionando integrado com banco MySQL

---
+++ api-authentication/src/test/java/br/com/studioSalon/apiAuthentication/ApiAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studioSalon.apiAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a12144ae357af54847e15a074143dfeaef2f4780
Wanderson Lira <wandersonlira.dev1@gmail.com>
2025-01-12 17:33:32 -0300

    :sparkles: feat: issue #1 - Implementado 'Permission', 'User' que refere-se ao usuário e sua respectiva permissão

---
+++ api-authentication/src/main/java/br/com/studioSalon/apiAuthentication/model/User.java
@@ -0,0 +1,174 @@
+package br.com.studioSalon.apiAuthentication.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Column(name = "user_name", unique = true) private String userName;
+    @Column(name = "full_name")  private String fullName;
+    @Column(name = "password")  private String password;
+    @Column(name = "account_non_expired") private Boolean accountNonExpired;
+    @Column(name = "account_non_locked") private Boolean accountNonLocked;
+    @Column(name = "credentials_non_expired") private Boolean credentialsNonExpired;
+    @Column(name = "enabled") private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = {@JoinColumn(name = "id_user")},
+            inverseJoinColumns = {@JoinColumn(name = "id_permission")}
+    ) private List<Permission> permissions;
+
+
+
+    public User() {}
+
+
+    public List<String> getRoles() {
+        List<String> roles = new ArrayList<>();
+        for (Permission permission : this.permissions) {
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Boolean getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public Boolean getAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(Boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public Boolean getAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(Boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return id.equals(user.id) && userName.equals(user.userName) && fullName.equals(user.fullName) && password.equals(user.password) && accountNonExpired.equals(user.accountNonExpired) && accountNonLocked.equals(user.accountNonLocked) && credentialsNonExpired.equals(user.credentialsNonExpired) && enabled.equals(user.enabled) && permissions.equals(user.permissions);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + userName.hashCode();
+        result = 31 * result + fullName.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + accountNonExpired.hashCode();
+        result = 31 * result + accountNonLocked.hashCode();
+        result = 31 * result + credentialsNonExpired.hashCode();
+        result = 31 * result + enabled.hashCode();
+        result = 31 * result + permissions.hashCode();
+        return result;
+    }
+}

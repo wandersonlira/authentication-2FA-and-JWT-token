c01fcb6cf7a73740a1169e6bed13571e3327fb87
Wanderson Lira <wandersonlira.dev1@gmail.com>
2025-01-30 21:22:42 -0300

    :sparkles: feat: Ajustes temporários para integrar o banco de dados com a regra de negócio

--- api-authentication/src/main/resources/db/migration/V7__Create_Table_Users_Customer.sql
+++ api-authentication/src/main/resources/db/migration/V7__Create_Table_Users_Customer.sql
@@ -4,7 +4,10 @@ CREATE TABLE IF NOT EXISTS `users_customer` (
     `email` VARCHAR(63),
     `phone_number` VARCHAR(15),
     `active` CHAR(1),
+    `user_token_id` bigint,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_email` (`email`),
-    UNIQUE KEY `uk_phone_number` (`phone_number`)
+    UNIQUE KEY `uk_phone_number` (`phone_number`),
+    CONSTRAINT `fk_users_customer_user_token_id` FOREIGN KEY (`user_token_id`)
+    REFERENCES `users` (`id`) -- ON DELETE CASCADE
 ) ENGINE=InnoDB;
---
+++ api-authentication/src/main/resources/db/migration/V8__Insert_Data_In_Users_Customer.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users_customer` (name, email, phone_number, active, user_token_id)
+    VALUES ('lira', 'email@gmail.com', '5581012345678', 'S', 1);
